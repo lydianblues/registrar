0d5833782713bc8e1de702f111caf441b557a7cc
lydianblues <lydianblues@gmail.com>
2016-07-17 14:21:19 -0700

    Fix nav menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+	# <li class="nav-item">
+	#          <%= link_to_unless_current("Courses", courses_path, {class: "nav-link"}) do %>
+	#              <span class="nav-link active">Courses</span>
+	#          <% end %>
+	#    </li>
+ 	def nav_menu_item(name, path)
+ 		content_tag(:li, class: "nav-item") do 
+ 			link_to_unless_current(name, path, {class: "nav-link"}) do
+ 				content_tag(:span, name, {class: "nav-link active"})
+ 			end
+ 		end
+ 	end
+
 end
